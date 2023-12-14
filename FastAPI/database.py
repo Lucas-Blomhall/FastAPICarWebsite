7b752139ca38901fde47f207e482e1559ca2dd83
Lucas-Blomhall <91318466+Lucas-Blomhall@users.noreply.github.com>
2023-12-14 14:03:30 +0100

    Removed comments

--- FastAPI/database.py
+++ FastAPI/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 # from database_models import SQLCar
 
-DATABASE_URL = "postgresql://postgres:Vanligt123!@localhost/carappdb"
+DATABASE_URL = "postgresql://postgres:YourPassword@localhost/carappdb"
 
 engine = create_engine(DATABASE_URL)
 Base = declarative_base()
@@ -19,16 +19,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
-# No test
-# try:
-#     # här försöker nå till databasen och få ett meddelande ifall vi lyckades eller inte
-#     with engine.connect() as connection:
-#         result = connection.execute(SQLCar)
-#         for row in result:
-#             print(row)
-#     print("Database connection was successful.")
-# except Exception as e:
-#     print("Error connecting to the database:", e)
-
-# Use this dependency in your route handlers
